1f1f4cad90193345ee59154a167faf767c320db6
dxiaoping <1693547683@qq.com>
2020-09-12 12:48:36 +0800

    引入color UI

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import App from './App'
 
 // Vue.config.productionTip = false
 // App.mpType = 'app'
-import '../static/colorui/main.wxss';
-import '../static/colorui/icon.wxss';
-import '../static/colorui/animation.wxss';
+import '../colorui/main.wxss';
+import '../colorui/icon.wxss';
+import '../colorui/animation.wxss';
 const app = new Vue(App)
 app.$mount()
 
--- src/pages/myInfo/myInfo.vue
+++ src/pages/myInfo/myInfo.vue
@@ -27,6 +27,7 @@
       <van-cell icon="shop-o" title="周计划管理" size="large" value="内容" is-link />
       <van-cell icon="shop-o" title="资金管理" size="large" value="内容" is-link />
       <van-cell icon="shop-o" title="人情管理" size="large" value="内容" is-link />
+      <van-cell is-link icon="shop-o" title="时间轴" size="large" value="时间轴" link-type="navigateTo" url="/pages/timeAxis/main"/>
       <van-cell icon="shop-o" title="摄影" size="large" value="内容" is-link />
       <van-cell title="单元格" value="内容" size="large" label="描述信息" :border="false" is-link />
     </van-cell-group>
