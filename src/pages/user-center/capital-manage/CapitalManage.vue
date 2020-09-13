322a5d0f0ec208b2eb0b8f097fc48eed587d6455
dxiaoping <1693547683@qq.com>
2020-09-13 11:10:46 +0800

    用户中心目录搭建

--- src/pages/user-center/capital-manage/CapitalManage.vue
+++ src/pages/user-center/capital-manage/CapitalManage.vue
@@ -1,7 +1,102 @@
 <!--资金管理-->
 <template>
   <div>
-  该模块正在开发，感谢您的关注与支持
+    <view class="cu-form-group">
+      <view class="title">日期选择</view>
+      <picker
+        mode="date"
+        :value="date"
+        start="2015-09-01"
+        end="2020-09-01"
+        bindchange="DateChange"
+      >
+        <view class="picker" text>{{date}}</view>
+      </picker>
+    </view>
+
+    <view class="cu-bar bg-white margin-top solid-bottom">
+      <view class="action">
+        <text class="cuIcon-title text-blue"></text>资金统计
+      </view>
+    </view>
+    <view class="padding bg-white">
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">薪资收入</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">本月支出</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">本月存储</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+    </view>
+
+    <view class="cu-bar bg-white margin-top solid-bottom">
+      <view class="action">
+        <text class="cuIcon-title text-blue"></text>资金规划
+      </view>
+    </view>
+    <view class="padding bg-white">
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">薪资收入</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">本月支出</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">本月存储</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+    </view>
+
+    <view class="cu-bar bg-white margin-top solid-bottom">
+      <view class="action">
+        <text class="cuIcon-title text-blue"></text>预算差异
+      </view>
+    </view>
+    <view class="padding bg-white">
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">薪资收入</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">本月支出</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+      <view class="flex">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius text-center">本月存储</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+    </view>
+    <!--
+  
+    <view class="padding bg-white">
+      <view class="flex p-xs margin-bottom-sm mb-sm">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius">1</view>
+        <view class="flex-twice bg-grey padding-sm margin-xs radius">2</view>
+      </view>
+      <view class="flex p-xs margin-bottom-sm mb-sm">
+        <view class="flex-sub bg-grey padding-sm margin-xs radius">1</view>
+        <view class="flex-twice bg-grey padding-sm margin-xs radius">2</view>
+        <view class="flex-treble bg-grey padding-sm margin-xs radius">3</view>
+      </view>
+
+      <view class="flex flex-wrap">
+        <view class="basis-xs bg-grey margin-xs padding-sm radius">xs(20%)</view>
+        <view class="basis-df"></view>
+        <view class="basis-sm bg-grey margin-xs padding-sm radius">sm(40%)</view>
+        <view class="basis-df"></view>
+        <view class="basis-df bg-grey margin-xs padding-sm radius">sub(50%)</view>
+        <view class="basis-lg bg-grey margin-xs padding-sm radius">lg(60%)</view>
+        <view class="basis-xl bg-grey margin-xs padding-sm radius">xl(80%)</view>
+      </view>
+    </view>
+    -->
   </div>
 </template>
 
